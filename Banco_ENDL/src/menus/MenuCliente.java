cd5248792e2e159fc33313aaf0a9348beb6c9545
Leonardo Lopez <lopezmezaleonardo553@gmail.com>
2024-11-21 09:13:54 -0600

    Menu del cliente agg

--- Banco_ENDL/src/menus/MenuCliente.java
+++ Banco_ENDL/src/menus/MenuCliente.java
@@ -1,4 +1,54 @@
 package menus;
 
+import usuarios.Cliente;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MenuCliente {
+    public ArrayList<Cliente> clientes = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public void mostrarMenuCliente() {
+        for (Cliente cliente : clientes) {
+            if (cliente.getSaldo() < 30000) {
+                int opcion = 6;
+
+                while (opcion != 4) {
+                    System.out.println("""
+                            1.- Hacer deposito
+                            2.- Hacer retiro
+                            3.- Saldo de cuenta
+                            4.- Salir""");
+                    opcion = sc.nextInt();
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.print("Digite la cantidad a depositar: ");
+                            double deposito = sc.nextDouble();
+
+                            break;
+                        case 2:
+                            System.out.print("Digite la cantidad a retirar: ");
+                            double retiro = sc.nextDouble();
+                            break;
+                        case 3:
+                            System.out.println("***** SALDO DE LA CUENTA *****");
+                            break;
+                        case 4:
+                            break;
+                    }
+                }
+            } else {
+                System.out.println("""
+                        1.- Hacer deposito
+                        2.- Hacer retiro
+                        3.- Saldo de cuenta
+                        5.- Solicitar tarjeta de debito
+                        6.- Hacer compra
+                        4.- Salir""");
+            }
+        }
+    }
 }
+
--- Banco_ENDL/src/usuarios/Cliente.java
+++ Banco_ENDL/src/usuarios/Cliente.java
@@ -10,8 +10,9 @@ public class Cliente {
     public String email;
     public LocalDate fechaRegistro;
     public String sucursal;
+    public double saldo;
 
-    public Cliente(String nombre, String apellido, String RFC, String CURP, String email, LocalDate fechaRegistro, String sucursal) {
+    public Cliente(String nombre, String apellido, String RFC, String CURP, String email, double saldo) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.RFC = RFC;
@@ -19,7 +20,70 @@ public class Cliente {
         this.email = email;
         this.fechaRegistro = LocalDate.now();
         this.sucursal = "GERMANS CUMS";
+        this.saldo = saldo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public String getCURP() {
+        return CURP;
+    }
+
+    public void setCURP(String CURP) {
+        this.CURP = CURP;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(String sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
